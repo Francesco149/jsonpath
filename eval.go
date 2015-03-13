4f25377fa39212cc0849501e5402ac4b40d85e73
Nick Sardo <nicksardo@gmail.com>
2015-03-12 17:36:29 -0700

    Move result into own file

--- eval.go
+++ eval.go
@@ -6,21 +6,6 @@ import (
 	"fmt"
 )
 
-const (
-	JsonObject = iota
-	JsonArray
-	JsonString
-	JsonNumber
-	JsonNull
-	JsonBool
-)
-
-type Result struct {
-	Keys  []interface{}
-	Value []byte
-	Type  int
-}
-
 type queryStateFn func(*query, *Item) queryStateFn
 
 type query struct {
@@ -405,57 +390,4 @@ func itemMatchOperator(loc interface{}, i *Item, op *operator) bool {
 		}
 	}
 	return false
-}
-
-func determineType(val []byte) int {
-	switch val[0] {
-	case '{':
-		return JsonObject
-	case '"':
-		return JsonString
-	case '[':
-		return JsonArray
-	case 'n':
-		return JsonNull
-	case 't', 'b':
-		return JsonBool
-	default:
-		return JsonNumber
-	}
-}
-
-func (r *Result) Pretty(showPath bool) string {
-	b := bytes.NewBufferString("")
-	printed := false
-	if showPath {
-		for _, k := range r.Keys {
-			switch v := k.(type) {
-			case int:
-				b.WriteString(fmt.Sprintf("%d", v))
-			default:
-				b.WriteString(fmt.Sprintf("%q", v))
-			}
-			b.WriteRune('\t')
-			printed = true
-		}
-	} else if r.Value == nil {
-		if len(r.Keys) > 0 {
-			printed = true
-			switch v := r.Keys[len(r.Keys)-1].(type) {
-			case int:
-				b.WriteString(fmt.Sprintf("%d", v))
-			default:
-				b.WriteString(fmt.Sprintf("%q", v))
-			}
-		}
-	}
-
-	if r.Value != nil {
-		printed = true
-		b.WriteString(fmt.Sprintf("%s", r.Value))
-	}
-	if printed {
-		b.WriteRune('\n')
-	}
-	return b.String()
 }
---
+++ result.go
@@ -0,0 +1,70 @@
+package jsonpath
+
+
+const (
+	JsonObject = iota
+	JsonArray
+	JsonString
+	JsonNumber
+	JsonNull
+	JsonBool
+)
+
+type Result struct {
+	Keys  []interface{}
+	Value []byte
+	Type  int
+}
+
+func (r *Result) Pretty(showPath bool) string {
+	b := bytes.NewBufferString("")
+	printed := false
+	if showPath {
+		for _, k := range r.Keys {
+			switch v := k.(type) {
+			case int:
+				b.WriteString(fmt.Sprintf("%d", v))
+			default:
+				b.WriteString(fmt.Sprintf("%q", v))
+			}
+			b.WriteRune('\t')
+			printed = true
+		}
+	} else if r.Value == nil {
+		if len(r.Keys) > 0 {
+			printed = true
+			switch v := r.Keys[len(r.Keys)-1].(type) {
+			case int:
+				b.WriteString(fmt.Sprintf("%d", v))
+			default:
+				b.WriteString(fmt.Sprintf("%q", v))
+			}
+		}
+	}
+
+	if r.Value != nil {
+		printed = true
+		b.WriteString(fmt.Sprintf("%s", r.Value))
+	}
+	if printed {
+		b.WriteRune('\n')
+	}
+	return b.String()
+}
+
+func determineType(val []byte) int {
+	switch val[0] {
+	case '{':
+		return JsonObject
+	case '"':
+		return JsonString
+	case '[':
+		return JsonArray
+	case 'n':
+		return JsonNull
+	case 't', 'b':
+		return JsonBool
+	default:
+		return JsonNumber
+	}
+}
